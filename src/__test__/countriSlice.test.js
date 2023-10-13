3a6c934acd9d327bebbf2bc5906166a39dfb1ec6
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-10-13 08:24:49 +0600

    Finish test of all reducers

--- src/__test__/countriSlice.test.js
+++ src/__test__/countriSlice.test.js
@@ -9,4 +9,68 @@ describe('test all reducer', () => {
       error: '',
     });
   });
+
+  it('test all countries', () => {
+    const payload = [{
+      name: { common: 'Country 1' },
+      area: 100,
+      cca3: 'ABC',
+      flags: { svg: 'flag-url' },
+      name: { official: 'Official Name' }
+    }];
+    const state = countryReducer(initialState, allCountries.fulfilled(payload));
+    expect(state.loading).toBe(false);
+    expect(state.regionalCountries.length).toEqual(1);
+    expect(state.error).toEqual('');
+  })
+  
+  it('test regional countries', () => {
+    const payload = [{
+      name: { common: 'Country 1' },
+      area: 100,
+      cca3: 'ABC',
+      flags: { svg: 'flag-url' },
+      name: { official: 'Official Name' }
+    },
+    {
+      name: { common: 'Country 1' },
+      area: 100,
+      cca3: 'ABC',
+      flags: { svg: 'flag-url' },
+      name: { official: 'Official Name' }
+    }];
+    const state = countryReducer(initialState, searchRegion.fulfilled(payload));
+    expect(state.loading).toBe(false);
+    expect(state.regionalCountries.length).toEqual(2);
+    expect(state.error).toEqual('');
+  })
+
+  it('test country details', () => {
+    const payload = [{
+      name: { common: 'Country 1' },
+      area: 100,
+      flags: { svg: 'flag-url' },
+      region: 'Region 1',
+      subregion: 'Subregion 1',
+      population: 1000000,
+      timezones: ['UTC'],
+      startOfWeek: 'Sunday',
+      official: 'Official Name',
+    }];
+    const state = countryReducer(initialState, countryDetails.fulfilled(payload));
+    expect(state.loading).toBe(false);
+    expect(state.singleCountry).toEqual(
+      {
+        "area": 100,
+        "flag": "flag-url",
+        "name": "Country 1",
+        "official": undefined,
+        "population": 1000000,
+        "region": "Region 1",
+        "startOfWeek": "Sunday",
+        "subregion": "Subregion 1", 
+        "timezones": "UTC"}
+    );
+    expect(state.error).toEqual('');
+  })
 });
